af6a1d8dad832fcffdbfe645f49ce5b72836f222
Vitor Raphael Gil <vitorraphaelbank@gmail.com>
2026-07-28 18:34:36 -0300

    chore: estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/vitorraphael/ifood/merchant/api/Application.java
@@ -0,0 +1,13 @@
+package com.vitorraphael.ifood.merchant.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
